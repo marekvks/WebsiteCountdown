76b981b888841abda35df7bb3aacad6b056295f0
Marek <84468808+marekvks@users.noreply.github.com>
2022-01-28 08:40:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var finishDate = new Date("Jan 12, 2022 00:00:00").getTime();
+var finishDate = new Date("Jan 12, 2022 00:00:00").getTime()
 
-var x = setTimeout(function () {
-    var now = new Date().getTime();
+var x = setTimeout(() => {
+    var now = new Date().getTime()
 
-    var distance = finishDate - now;
+    var distance = finishDate - now
 
-    var days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+    var days = Math.ceil(distance / (1000 * 60 * 60 * 24))
 
-    document.getElementById("time").innerHTML = days;
+    document.getElementById("time").innerHTML = days
 }, 0);
